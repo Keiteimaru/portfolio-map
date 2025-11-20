13877bb5656a4cdc05575e329f0a5529ab50e802
keiteimaru <213720103+Keiteimaru@users.noreply.github.com>
2025-11-20 20:47:09 +0900

    [Add]Vercel用のパスワード設定追加-3

--- middleware.js
+++ middleware.js
@@ -1,27 +1,20 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-  const basicAuth = req.headers.get("authorization");
+  const auth = req.headers.get('authorization');
 
-  // ユーザー名とパスワードは環境変数から
-  const user = process.env.BASIC_USER;
-  const pass = process.env.BASIC_PASS;
+  const USER = process.env.BASIC_USER;
+  const PASS = process.env.BASIC_PASS;
 
-  // 認証がある場合のチェック
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
-    const [inputUser, inputPass] = atob(authValue).split(":");
-
-    if (inputUser === user && inputPass === pass) {
+  if (auth) {
+    const [user, pass] = atob(auth.split(' ')[1]).split(':');
+    if (user === USER && pass === PASS) {
       return NextResponse.next();
     }
   }
 
-  // 認証失敗 → ベーシック認証を要求
-  return new NextResponse("Authentication required", {
+  return new NextResponse('Auth required', {
     status: 401,
-    headers: {
-      "WWW-Authenticate": `Basic realm="Secure Area"`,
-    },
+    headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
   });
 }
